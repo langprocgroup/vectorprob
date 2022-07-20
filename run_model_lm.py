7a73996e3ae8ca4140903fd939fa73ced589eacc
Richard Futrell <rfutrell@uci.edu>
2022-07-20 13:38:04 -0700

    add model runner for forms rather than bigrams

---
+++ run_model_lm.py
@@ -0,0 +1,20 @@
+import sys
+import csv
+
+import torch
+
+import readwrite as rw
+from bilinear import WordVectors, MarginalLogLinear, ConditionalSoftmax, ConditionalLogBilinear
+
+def main(model_filename, data_filename):
+    model = torch.load(model_filename)
+    with open(data_filename) as infile:
+        for form in infile:
+            word = ['#'] + form.strip().split() + ['#']
+            pairs = zip(word, word[1:])
+            score = model(pairs).sum()
+            writer.writerow([form, score])
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
+    
